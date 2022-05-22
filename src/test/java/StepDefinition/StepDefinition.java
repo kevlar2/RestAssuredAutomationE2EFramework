b4333307f3260bd3904a7847e32435f4b48d3f96
Kevin O <korukele84@outlook.com>
2022-05-22 13:54:41 +0100

    Changed line

--- src/test/java/StepDefinition/StepDefinition.java
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -1,6 +1,6 @@
 package StepDefinition;
 
-eimport io.cucumber.java.en.Given;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
