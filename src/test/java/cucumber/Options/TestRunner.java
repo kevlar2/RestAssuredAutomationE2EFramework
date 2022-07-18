9cad173bfc5b378115e573a8f360c58d7d6564a6
MacbookPro-Kevlar <korukele84@outlook.com>
2022-07-18 19:15:38 +0100

    Add reporting feature to code

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -6,8 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features ="src/test/java/features",
+        plugin = "json:target/jsonReports/cucumber-reports.json" ,
         glue = {"StepDefinition"},
-        tags = "@AddPlace")
+        tags = "")
 public class TestRunner {
 
 }
